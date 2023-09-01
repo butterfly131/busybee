4685d49e2c49ee3b60976bfcd4e69cc57354d310
butterfly-131 <bzbutterfly3@gmail.com>
2023-09-01 17:41:58 +0500

    Backend updated

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,17 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
-
 import postRoutes from './routes/posts.js';
 
 const app = express();
 dotenv.config();
 console.log("test");
 
-app.use(cors());
+// const dotenv = require("dotenv");
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
 
 app.use('/', postRoutes);
 
@@ -28,6 +28,7 @@ app.use(cors({
 }));
 
 const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = process.env.MONGO_URL
 
 mongoose.connect("mongodb+srv://fatim:12345@cluster0.jj5p27d.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Success! Server Running on Port: http://localhost:${PORT}`)))
